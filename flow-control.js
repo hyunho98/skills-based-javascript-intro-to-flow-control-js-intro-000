5d2101e38009299933663f9562a7121170dc9537
Alex Lee <hyunho98@gmail.com>
2019-03-25 01:42:17 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -3,7 +3,7 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-  if(age > 13 && age < 19){
+  if(age > 12 && age < 20){
     return "You are a teenager!"
   }
   else{
@@ -12,7 +12,15 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
+  if (age < 13){
+    return "You are a kid"
+}
+  else if(age > 19){
+    return "You are a grownup"
+}
+  else{
+    return "You are a teenager!"
+}
 }
 
 function ternaryTeenager(age) {
